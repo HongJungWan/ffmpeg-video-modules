6d033c1ce3f5a4287c24315156d95e8e74422a38
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-08-07 18:09:41 +0900

    feat: health check API 구현

---
+++ app/interfaces/health_check_controller.go
@@ -0,0 +1,20 @@
+package interfaces
+
+import (
+	"github.com/HongJungWan/ffmpeg-video-modules/app/usecases"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type HealthCheckController struct {
+	interactor usecases.HealthCheckInteractor
+}
+
+func NewHealthCheckController(interactor usecases.HealthCheckInteractor) *HealthCheckController {
+	return &HealthCheckController{interactor}
+}
+
+func (h *HealthCheckController) HealthCheck(c *gin.Context) {
+	result := h.interactor.PerformHealthCheck()
+	c.JSON(http.StatusOK, result)
+}
