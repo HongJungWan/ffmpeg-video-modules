95c285aa5bc75b3df86c6cd6092f7dfb98586fcc
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-08-09 13:45:06 +0900

    feat: Video Upload 시, 동영상 길이 저장하는 기능 구현

--- app/usecases/video_interactor.go
+++ app/usecases/video_interactor.go
@@ -3,6 +3,7 @@ package usecases
 import (
 	"fmt"
 	"github.com/HongJungWan/ffmpeg-video-modules/app/domain"
+	"github.com/HongJungWan/ffmpeg-video-modules/app/infrastructure/ffmpeg"
 	"github.com/HongJungWan/ffmpeg-video-modules/app/interfaces/dto"
 	"github.com/HongJungWan/ffmpeg-video-modules/app/interfaces/repository"
 	"github.com/gin-gonic/gin"
@@ -41,8 +42,14 @@ func (vi *VideoInteractorImpl) HandleVideoUpload(ctx *gin.Context) error {
 			return fmt.Errorf("파일을 저장할 수 없음: %v", err)
 		}
 
+		// 동영상 길이 계산
+		duration, err := ffmpeg.GetVideoDuration(dst)
+		if err != nil {
+			return fmt.Errorf("동영상 길이를 계산할 수 없음: %v", err)
+		}
+
 		// 새로운 비디오 엔트리 생성
-		videoResponse, err := vi.processVideo(file.Filename, dst)
+		videoResponse, err := vi.processVideo(file.Filename, dst, duration)
 		if err != nil {
 			return err
 		}
@@ -52,8 +59,8 @@ func (vi *VideoInteractorImpl) HandleVideoUpload(ctx *gin.Context) error {
 	return nil
 }
 
-func (vi *VideoInteractorImpl) processVideo(filename string, filePath string) (*dto.VideoResponse, error) {
-	video := domain.NewVideo(filename, filePath)
+func (vi *VideoInteractorImpl) processVideo(filename string, filePath string, duration int) (*dto.VideoResponse, error) {
+	video := domain.NewVideo(filename, filePath, duration)
 	if err := vi.VideoRepository.Save(video); err != nil {
 		return nil, fmt.Errorf("비디오를 생성할 수 없음: %v", err)
 	}
@@ -63,6 +70,7 @@ func (vi *VideoInteractorImpl) processVideo(filename string, filePath string) (*
 		ID:        video.ID,
 		Filename:  video.Filename,
 		FilePath:  video.FilePath,
+		Duration:  video.Duration,
 		Status:    string(video.Status),
 		CreatedAt: video.CreatedAt,
 		UpdatedAt: video.UpdatedAt,
