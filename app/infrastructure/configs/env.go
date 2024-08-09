12553377c7425f36464982c6333a1ce528d85ff3
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-08-09 09:24:04 +0900

    chore: 설정 파일 수정 및 의존성 추가 (#35)
    * refactor: 파일 구조 정리 및 파일 이동 (#32)
    
    - 사용하지 않는 파일 및 불필요한 파일 삭제
    - 파일 구조 변경에 따른 관련 파일의 경로 업데이트
    
    * chore: 설정 파일 수정 및 의존성 추가 (#34)
    
    - 프로젝트 설정 파일 (.gitignore, go.mod, config.toml 등) 수정
    - README.md 수정

--- app/infrastructure/configs/env.go
+++ app/infrastructure/configs/env.go
@@ -8,4 +8,9 @@ type Config struct {
 	DBPort     string `mapstructure:"mariadb_port"`
 
 	Environment string `toml:"ENVIRONMENT"`
+
+	AWSAccessKeyID     string `mapstructure:"aws_access_key_id"`
+	AWSSecretAccessKey string `mapstructure:"aws_secret_access_key"`
+	AWSRegion          string `mapstructure:"aws_region"`
+	S3BucketName       string `mapstructure:"s3_bucket_name"`
 }
