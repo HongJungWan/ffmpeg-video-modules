aef742e161356d40a21e5852b728cb3cc7b1982d
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-08-12 09:05:34 +0900

    docs: Swagger를 이용한 API 문서 자동화 (#79)

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/HongJungWan/ffmpeg-video-modules/cmd/docs"
 	"github.com/HongJungWan/ffmpeg-video-modules/cmd/helper"
 	"github.com/HongJungWan/ffmpeg-video-modules/cmd/infrastructure/configs"
 	"os"
@@ -16,6 +18,19 @@ func main() {
 		helper.ShowHelp()
 		os.Exit(-1)
 	}
+	initializeSwaggerHost(&conf)
 	db := configs.ConnectionDB(&conf)
 	startServer(db)
+}
+
+func initializeSwaggerHost(conf *configs.Config) {
+	var host string
+	var scheme []string
+
+	host = conf.Host
+	scheme = conf.Scheme
+
+	docs.SwaggerInfo.Host = host
+	docs.SwaggerInfo.Schemes = scheme
+	fmt.Printf("설정된 Swagger Host: %s, Schemes: %v \n", host, scheme)
 }
