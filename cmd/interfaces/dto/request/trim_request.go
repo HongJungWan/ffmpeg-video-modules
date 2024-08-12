2973b6b3e886ee92855affdeb56bda0dba106934
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-08-12 13:06:45 +0900

    refactor: trim request dto default 값 설정

--- cmd/interfaces/dto/request/trim_request.go
+++ cmd/interfaces/dto/request/trim_request.go
@@ -1,6 +1,6 @@
 package request
 
 type TrimVideoRequest struct {
-	TrimStart string `json:"trimStart" binding:"required"`
-	TrimEnd   string `json:"trimEnd" binding:"required"`
+	TrimStart string `json:"trimStart" default:"00:00:01"`
+	TrimEnd   string `json:"trimEnd" default:"00:00:03"`
 }
